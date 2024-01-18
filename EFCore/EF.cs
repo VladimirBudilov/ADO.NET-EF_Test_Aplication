1fe1d92d2180ed53d3a6e72020e2b43dff81313c
VladimirBudilov <109314834+VladimirBudilov@users.noreply.github.com>
2024-01-18 10:50:28 +0400

    add few nuget packages. in EF: add CRUD operations, logging .

--- EFCore/EF.cs
+++ EFCore/EF.cs
@@ -9,8 +9,10 @@ namespace ADO.NET_EF_Test_Aplication.EFCore
         {
             using (var context = new TestContext())
             {
+                if(!context.Database.CanConnect()) return;
                 context.Database.EnsureDeleted();
                 context.Database.EnsureCreated();
+                //Create
                 var toyota = new Car() { Name = "Toyota" };
                 context.Users.Add(new User() { Name = "John", Car = toyota });
                 context.Users.Add(new User() { Name = "Jane", Car =  toyota });
@@ -20,6 +22,23 @@ namespace ADO.NET_EF_Test_Aplication.EFCore
                     Console.WriteLine($"{user.Id}: {user.Name}, {user.Car.Name}");
                 }
                 Console.WriteLine("");
+                //Update
+                var ford = new Car() {Name = "Ford"};
+                context.Users.Add(new User() { Name = "John", Car = ford });
+                context.SaveChanges();
+                var lastUser = context.Users.ToList().Last();
+                lastUser.Name = "Jons";
+                context.Users.Update(lastUser);
+                context.SaveChanges();
+                //Delete
+                context.Users.Add(new User()
+                {
+                    Name = "Jane", Car = ford,
+                });
+                context.SaveChanges();
+                context.Users.Remove(context.Users.ToList().Last());
+                context.SaveChanges();
+                //Read
                 foreach (var car in context.Car)
                 {
                     Console.WriteLine($"{car.Users.Count}: {car.Name}");
--- EFCore/EfContext.cs
+++ EFCore/EfContext.cs
@@ -1,16 +1,19 @@
 using ADO.NET_EF_Test_Aplication.EFCore.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace ADO.NET_EF_Test_Aplication.EFCore
 {
     public sealed class TestContext : DbContext
     {
         public TestContext() => Database.EnsureCreated();
+        readonly StreamWriter _writer = new StreamWriter("log.txt");
         public DbSet<User> Users { get; set; }
         public DbSet<Car> Car { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Data Source=./userEFsdata.db");
+            optionsBuilder.LogTo(_writer.WriteLine, LogLevel.Information);
         }
             
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -19,6 +22,12 @@ namespace ADO.NET_EF_Test_Aplication.EFCore
                 .HasOne<Car>(s => s.Car)
                 .WithMany(g => g.Users)
                 .HasForeignKey(s => s.CarId);
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            _writer.Dispose();
         }
     }
 }
